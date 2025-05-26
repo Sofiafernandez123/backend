8ed7f82c245e4184bede2d27da7d552b571c25c3
Sofiafernandez123 <sofifernandeze@gmail.com>
2025-05-25 22:26:25 -0300

    cambie appport por port en server

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 // ======================
 // Configuration
 // ======================
-const PORT = process.env.APP_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 // ======================
